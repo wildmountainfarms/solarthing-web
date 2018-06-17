e3a7fbebdbcf2551ae2fa8f944f3f2b3effdd219
retrodaredevil <retrodaredevil@gmail.com>
2018-06-16 23:19:41 -0500

    Added data on left, started graph on right

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # solarthing-web
 The web app to show how much power is being used at Wild Mountain Farms
 
-The web partion of this application was never uploaded to github but you can see the backend here: https://github.com/eidolon1138/solarthing
+See the backend here: https://github.com/eidolon1138/solarthing
--- index.js
+++ index.js
@@ -1,46 +1,79 @@
 google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.setOnLoadCallback(drawLogScales);
 
+db = null;
+try{
+
+} catch(e){
+	console.log("unable to initialize database. That's not very relaxing!");
+}
+
 function drawLogScales() {
+	let element = document.getElementById("chart_div");
 	let data = new google.visualization.DataTable();
-	data.addColumn('number', 'X');
-	data.addColumn('number', 'Power coming in');
-	data.addColumn('number', 'Power being used');
+	data.addColumn('string', 'X');
+	data.addColumn('number', 'Battery Voltage');
+	data.addColumn('number', 'Panel Voltage');
 
 	data.addRows([
-		[0, 0, 0], [1, 10, 5], [2, 23, 15], [3, 17, 9], [4, 18, 10], [5, 9, 5]
+		["0:00", 0, 0], ["1:00", 10, 5], ["2:00", 23, 15], ["3:00", 17, 9], ["4:00", 18, 10], ["5:00", 9, 5]
 	]);
 
 	let options = {
+	    title: "Cool Title",
+        titleTextStyle: {
+			color: "#000000"
+        },
 		hAxis: {
 			title: 'Time',
-			logScale: false
+			logScale: false,
+            textStyle: { color: "#000000" },
+            titleTextStyle: { color: "#000000" },
+            gridlines: { color: "#000000" }
 		},
 		vAxis: {
-			title: 'Popularity',
-			logScale: false
+			title: 'Voltage',
+			logScale: false,
+            textStyle: { color: "#000000" },
+            titleTextStyle: { color: "#000000" },
+            gridlines: { color: "#000000" }
 		},
+        lineWidth: 5,
 		colors: ['#a52714',
-			'#097138']
+			'#097138'],
+		backgroundColor: element.style["background-color"],
+		legend: {
+	    	textStyle: { color: "#000000" }
+		},
+		chartArea: { width: "50%" }
 	};
 
-	let chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+	let chart = new google.visualization.LineChart(element);
 	chart.draw(data, options);
 }
 function setBatteryVoltage(volts){
+    if(volts == null){
+    	volts = "?";
+	}
     document.getElementById("battery_voltage").innerHTML = volts;
 }
 function setPanelAmps(amps){
+	if(amps == null){
+		amps = "?";
+	}
     document.getElementById("panel_amps").innerHTML = amps;
 }
 function setPanelVolts(volts){
+	if(volts == null){
+		volts = "?";
+	}
     document.getElementById("panel_volts").innerHTML = volts;
 }
 
 
 function main(){
-	setBatteryVoltage(0);
-	setPanelAmps(0);
-	setPanelVolts(0);
+	setBatteryVoltage(null);
+	setPanelAmps(null);
+	setPanelVolts(null);
 }
 main();
